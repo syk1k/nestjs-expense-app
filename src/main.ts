f77c66b2376de8abca9eeab0bab6476370a9c8b8
syk1k <dambemondo@gmail.com>
2023-10-09 00:18:28 +0000

    Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
@@ -13,6 +14,16 @@ async function bootstrap() {
       }
     })
   )
+
+  const config = new DocumentBuilder()
+    .setTitle('Expense/Income report App')
+    .setDescription('The Expense/Income API description')
+    .setVersion('1.0')
+    .addTag('report')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
